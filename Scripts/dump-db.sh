2ef8a5ebb4a9dd23959235e1e62a3cf3295e47c4
kahnu044 <kahnucharan044@gmail.com>
2025-01-21 11:35:57 +0530

    dump and reset db updated

--- Scripts/dump-db.sh
+++ Scripts/dump-db.sh
@@ -2,4 +2,4 @@
 CWD=`CPWD=$(pwd);cd $(dirname \$0);pwd;cd \$CPWD`
 REPO_DIR=`dirname $CWD`
 source $REPO_DIR/.env
-docker exec -i ${COMPOSE_PROJECT_NAME}_db_1 mysqldump -uroot -proot $MYSQL_DATABASE > $REPO_DIR/dump/${COMPOSE_PROJECT_NAME}.sql
+docker exec -i ${COMPOSE_PROJECT_NAME}-db-1 mysqldump -uroot -proot $MYSQL_DATABASE > $REPO_DIR/dump/${COMPOSE_PROJECT_NAME}.sql
--- Scripts/reset-db.sh
+++ Scripts/reset-db.sh
@@ -2,5 +2,7 @@
 CWD=`CPWD=$(pwd);cd $(dirname \$0);pwd;cd \$CPWD`
 REPO_DIR=`dirname $CWD`
 source $REPO_DIR/.env
-echo "DROP DATABASE ${MYSQL_DATABASE}; CREATE DATABASE ${MYSQL_DATABASE};" | docker exec -i "${COMPOSE_PROJECT_NAME}_db_1" mysql -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}"
-pv $REPO_DIR/dump/${COMPOSE_PROJECT_NAME}.sql | docker exec -i "${COMPOSE_PROJECT_NAME}_db_1" mysql -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${MYSQL_DATABASE}"
+
+echo "DROP DATABASE ${MYSQL_DATABASE}; CREATE DATABASE ${MYSQL_DATABASE};" | docker exec -i "${COMPOSE_PROJECT_NAME}-db-1" mysql -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}"
+
+docker exec -i "${COMPOSE_PROJECT_NAME}-db-1" mysql -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${MYSQL_DATABASE}" < $REPO_DIR/dump/${COMPOSE_PROJECT_NAME}.sql
