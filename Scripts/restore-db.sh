034a8e40893f9126aa88ae5e35eb0f0bc3f89f8b
kahnu044 <kahnucharan044@gmail.com>
2025-01-21 11:44:07 +0530

    restore-db updated

--- Scripts/restore-db.sh
+++ Scripts/restore-db.sh
@@ -6,3 +6,5 @@ source $REPO_DIR/.env
 echo "DROP DATABASE ${MYSQL_DATABASE}; CREATE DATABASE ${MYSQL_DATABASE};" | docker exec -i "${COMPOSE_PROJECT_NAME}-db-1" mysql -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}"
 
 docker exec -i "${COMPOSE_PROJECT_NAME}-db-1" mysql -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${MYSQL_DATABASE}" < $REPO_DIR/dump/${COMPOSE_PROJECT_NAME}.sql
+
+echo "Thanks, Database restored successfully"
